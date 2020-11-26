1bd6ba5cd8d04695a32b89554b75c9bf77036f25
emiliogalera <emiliofgalera@gmail.com>
2020-11-26 17:03:48 -0300

    tested store_random_state

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main(){
     rate::ItineratedMap bla2(100, 0.1, 0.5);
     spiking::lif bla_lif;
 
+    bla.store_random_state(0.3);
+    bla2.store_random_state(0.3);
+
     bla_lif.hello("Hello from lif");
 
     return 0;
