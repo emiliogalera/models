813a43765dc5b1cb49f9608c2ec3eafa4991aac1
emiliogalera <emiliofgalera@gmail.com>
2021-01-22 10:00:50 -0300

    Cleaned the models files

--- models.h
+++ models.h
@@ -115,56 +115,6 @@ namespace rate{
             //Update the anti-Hebbian matrix
             void antiHebb_update();
 
-
-            /*ItineratedMap(int n_elements, float gma);
-            ItineratedMap(int n_elements, float a, float b, float gma);
-
-            // Define the parameters for the connection matrix (anti-hebbian term)
-            void anti_hebb_param(float _tau, float eps);
-
-            // Define parameter of the model (gamma)
-            void model_param(float gma);
-
-            // Store new random state function
-            void store_random_state(float a, float b);
-            void store_external_state(std::vector<float>& vec);
-            void store_pmone_random_state(float prob);
-
-            //Genarate a new initial random state
-            void state_zero_random();
-
-            //Generate a new initial state from user
-            void state_zero_external(std::vector<float> &state0);
-
-            //Calculate the Hebbian part of the connection matrix
-            void make_JHebbian();
-
-            //Make the initial state of the anti-Hebbian matrix
-            void anti_Hebb_init();
-
-            //Update the anti-Hebbian matrix
-            void anti_Hebb_update();
-
-            //Update the activity vector of the network h(t)
-            void activity_update();
-
-            //Update the state of the network
-            void state_update(); //without input
-            void state_update(std::vector<float>& input); //with input
-
-            // Probe function, gets a stored pattern
-            std::vector<float>& get_xi(int u);
-
-            //Probe state vector;
-            std::vector<float>& get_State();
-
-            //Probe activity
-            std::vector<float>& get_activity();
-
-            //Probe Hebb matrix
-            std::vector<std::vector<float>>& get_hebb();
-        */
-
     };
 }
 
