807a637eb499c7d4e4573a66eb6668bce6fe4d66
emiliogalera <emiliofgalera@gmail.com>
2021-01-27 09:15:07 -0300

    Test, bug!

--- test.cpp
+++ test.cpp
@@ -21,37 +21,45 @@ void print_var(const std::vector<std::vector<float>>& vec){
     std::cout << std::endl;
 }
 
-int main(){
+/*test call fingerprint:
+ *./test.out N Hebb/noHebb(0, 1) #of_strong_patterns F_strong #of_weak_patterns F_weak Prob
+ */
+int main(int argc, char* argv[]){
+
+    //convert commandline arguments
+    int N = std::stoi(argv[1]);
+    int boolHebb = std::stoi(argv[2]);
+    int nsp = std::stoi(argv[3]);
+    float fstrong = std::stof(argv[4]);
+    int nwp = std::stoi(argv[5]);
+    float fweak = std::stof(argv[6]);
+    float prob = std::stof(argv[7]);
 
     // Create a parameter variable for simulation
     rate::parameters simu_par;
-    simu_par.eps_ah = 0.02;
+    simu_par.eps_ah = 0.09; //0.02
     simu_par.eps_h = 0.001;
     simu_par.gamma = 10.0;
-    simu_par.tau = 100.0;
-    simu_par.N = 100;
+    simu_par.tau = 600.0; //100
+    simu_par.N = N; //might cause a warning!
 
     // Instanciate a ItineratedMap object with an initial state
     // in the continuous range (-1, 1)
-    rate::ItineratedMap bla2(simu_par, -1.0, 1.0);
+    rate::ItineratedMap rnn_model(simu_par, -1.0, 1.0);
 
-    // Stores 10 random patterns
-    bla2.add_random_pattern(0.5, 3.0);
-    bla2.add_random_pattern(0.5, 3.0);
-    bla2.add_random_pattern(0.5, 3.0);
-    bla2.add_random_pattern(0.5, 1.0);
-    bla2.add_random_pattern(0.5, 1.0);
-    bla2.add_random_pattern(0.5, 1.0);
-    //bla2.add_random_pattern(0.5, 1.0);
-    //bla2.add_random_pattern(0.5, 1.0);
-    //bla2.add_random_pattern(0.5, 1.0);
-    //bla2.add_random_pattern(0.5, 1.0);
-
+    // Stores P random patterns
+    //first stores the strong patterns
+    for(int i = 0; i != nsp; ++i){
+        rnn_model.add_random_pattern(prob, fstrong);
+    }
+    for(int i = 0; i != nwp; ++i){
+        rnn_model.add_random_pattern(prob, fweak);
+    }
     // Make the hebbian matrix based on the stored patterns
-    bla2.make_hebb_matrix();
+    rnn_model.make_hebb_matrix();
 
     // Initial activity
-    bla2.activity_update();
+    rnn_model.activity_update();
 
     // Number of iterations the simulation lasts
     int TIMER = 15000;
@@ -62,12 +70,13 @@ int main(){
 
     // Simple simulation scheme
     for(int t = 0; t != TIMER; ++t){
-        bla2.antiHebb_update();
-        bla2.hebb_update();
-        //bla2.state_update_tgh();
-        bla2.state_update_rational();
-        bla2.activity_update();
-        bla2.generate_m(m_vec);
+        rnn_model.antiHebb_update();
+        if(boolHebb){
+            rnn_model.hebb_update();
+        }
+        rnn_model.state_update_tgh();
+        rnn_model.activity_update();
+        rnn_model.generate_m(m_vec);
         for(float& mu : m_vec){
             std::cout << mu << " ";
         }
